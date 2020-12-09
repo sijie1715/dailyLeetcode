f89e6886a8adef611709bbeac462a495201700da
Sijie <Sijie@sijies-mac.lan>
2020-12-09 15:55:06 -0800

    added basic calculator 3

---
+++ 772. Basic Calculator III/Recursive.java
@@ -0,0 +1,25 @@
+class Solution {
+    int i = 0;
+    public int calculate(String s) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        int num = 0;
+        char op = '+';
+        while (i < s.length()) {
+            // update i here
+            char ch = s.charAt(i++);
+            if (Character.isDigit(ch)) {num = num * 10 + (ch - '0');}
+            if (ch == '(') {num = calculate(s);}
+            if (i >= s.length() || ch == '+' || ch == '-' 
+                || ch == '*' || ch == '/' || ch == ')') {
+                if (op == '+') stack.push(num);
+                else if (op == '-') stack.push(-num);
+                else if (op == '*') stack.push(stack.pop() * num);
+                else if (op == '/') stack.push(stack.pop() / num);
+                op = ch;
+                num = 0;
+            }
+            if (ch == ')') break;
+        }
+        return stack.stream().mapToInt(x -> x).sum();
+    }
+}
