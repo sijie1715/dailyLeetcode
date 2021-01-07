d96bf36088cae5ffba45fa43fdca2246e49371b9
sijie <sijie@hismac>
2021-01-07 00:50:40 -0800

    2020/10/18

---
+++ P416_PartitionEqualSubsetSum.java
@@ -0,0 +1,21 @@
+package Solutions;
+
+public class P416_PartitionEqualSubsetSum {
+    public boolean canPartition(int[] nums) {
+        if (nums.length == 1) return false;
+
+        int sum = 0;
+        for (int num : nums) sum += num;
+        if (sum % 2 != 0) return false;
+
+        sum /= 2;
+        boolean[] dp = new boolean[sum + 1];
+        dp[0] = true;
+        for (int num : nums) {
+            for (int i = sum; i >= num; i--) {
+                dp[i] |= dp[i - num];
+            }
+        }
+        return dp[sum];
+    }
+}
