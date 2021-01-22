9ac00cd9be7863f0e2fc881e2f46bfdfe924c7d4
sijie <sijie@hismac>
2021-01-21 23:08:50 -0800

    2020/11/24

---
+++ P503_NextGreaterElementII.java
@@ -0,0 +1,20 @@
+package Solutions;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class P503_NextGreaterElementII {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Arrays.fill(res, -1);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < n * 2; i++) {
+            while (!stack.isEmpty() && nums[i % n] > nums[stack.peek()]) {
+                res[stack.pop()] = nums[i % n];
+            }
+            stack.push(i % n);
+        }
+        return res;
+    }
+}
