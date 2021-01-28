97ade5da88949287eb69af3f5a001368dd9cb1ec
sijie <sijie@hismac>
2021-01-27 23:46:11 -0800

    2020/12/02

---
+++ P155_MinStack.java
@@ -0,0 +1,38 @@
+package Solutions;
+
+import java.util.Stack;
+
+public class P155_MinStack {
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    /**
+     * initialize your data structure here.
+     */
+    public P155_MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        if (minStack.isEmpty()) {
+            minStack.push(x);
+        } else {
+            minStack.push(Math.min(x, minStack.peek()));
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
