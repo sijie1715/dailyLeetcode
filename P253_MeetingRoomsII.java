9688863e0289828d40d7e68fbcee42eacd2221d7
sijie <sijie@hismac>
2021-03-08 20:00:47 -0800

    2021/03/08

---
+++ P253_MeetingRoomsII.java
@@ -0,0 +1,18 @@
+package Solutions;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+public class P253_MeetingRoomsII {
+    public int minMeetingRooms(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int[] interval : intervals) {
+            if (!pq.isEmpty() && interval[0] >= pq.peek()) {
+                pq.poll();
+            }
+            pq.add(interval[1]);
+        }
+        return pq.size();
+    }
+}
